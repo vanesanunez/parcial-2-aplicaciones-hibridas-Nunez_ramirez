61d6578f7ffb2dbb75642b475b455926c464284d
vanesanunez <142461430+vanesanunez@users.noreply.github.com>
2025-06-21 18:29:06 -0300

    rutas

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,8 +1,8 @@
 import {Routes, Route} from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home/Home'
-import Login from './pages/Login'
-import Register from './pages/Register';
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register';
 
 function App() {
   
@@ -10,8 +10,8 @@ function App() {
     <>
      <Routes>
       <Route path="/home" element={<Home/>}></Route>
-      <Route path="/" element={<Login/>}></Route>
       <Route path="/register" element={<Register />} />
+      <Route path="/" element={<Login/>}></Route>
      </Routes>
     </>
   )
