79df6157d05c025913e6fa4e05b4f39353bb34ed
vanesanunez <142461430+vanesanunez@users.noreply.github.com>
2025-06-23 01:56:08 -0300

    tengo conflicto con los estilos, revisa sass-embedded

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,7 +4,7 @@ import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register';
 import Rutas from './pages/Rutas.jsx/Rutas';
-import Reports from './pages/Reports.jsx/Reports'
+import ReportList from './pages/ReportList.jsx/ReportList';
 
 function App() {
   
@@ -12,7 +12,7 @@ function App() {
     <>
      <Routes>
       <Route path="/" element={<Home/>}></Route>
-      <Route path="/reports/:id" element={<Reports/>}></Route>
+      <Route path="/reports/:id" element={<ReportList/>}></Route>
       <Route path="/register" element={<Register/>} />
       <Route path="/login" element={<Login/>}></Route>
       <Route path="/rutas" element={<Rutas />} />
