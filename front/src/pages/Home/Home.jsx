3704772b91896e52535b600b96b55f0f8262cbed
tatilai <ramirezlaila.t23@gmail.com>
2025-06-22 22:31:40 -0300

    update home.jsx

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -4,8 +4,7 @@ import axios from "axios"
 import useDebounce from '../../hooks/useDebounce'
 import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom'
-
-//import '../../styles/home.scss';
+//import '../styles/home.scss';
 
 
 const Home = () => {
